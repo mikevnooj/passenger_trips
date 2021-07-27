e15ac2b2e5aa8e973051230e481bfffc8faa8b17
Mike Nugent, Jr <michaelvnugent@gmail.com>
2021-07-27 12:54:56 -0400

    start new weekly total problem

--- src/weekly_total_problem.R
+++ src/weekly_total_problem.R
@@ -1,11 +1,31 @@
-con_rep <- DBI::dbConnect(odbc::odbc(), Driver = "SQL Server", Server = "REPSQLP01VW", 
-                          Database = "TransitAuthority_IndyGo_Reporting", 
-                          Port = 1433)
+# okay couple of steps here
+# step 1. get stops from spreadsheet
+# step 2. get all routes to those stops during time period
+# step 3. read in boardings for every route for all stops
+# step 4. get transit_day, route, stop_id, n, uniquen trips
+# 
 
-VMH_Raw <- tbl(
-  con_rep,sql(
+
+
+library(magrittr)
+library(data.table)
+
+
+con_rep <- DBI::dbConnect(odbc::odbc()
+                          , Driver = "SQL Server"
+                          , Server = "REPSQLP01VW"
+                          , Database = "TransitAuthority_IndyGo_Reporting"
+                          , Port = 1433
+                          )
+
+
+
+
+
+VMH_Raw <- dplyr::tbl(
+  con_rep,dplyr::sql(
     paste0(
-      "select a.Time
+    "select a.Time
     ,a.Route
     ,Boards
     ,Alights
@@ -22,13 +42,13 @@ VMH_Raw <- tbl(
     from avl.Vehicle_Message_History a (nolock)
     left join avl.Vehicle_Avl_History b
     on a.Avl_History_Id = b.Avl_History_Id
-    where a.Route in (90,901,902,16,31)
+    where a.Route in (19,901,4,28,31)
     and a.Time > '20190901'
     and a.Time < DATEADD(day,1,'20200314')"
-  )
-  )
+    )
+    )
   ) %>%
-  collect() %>%
+  dplyr::collect() %>%
   data.table()
 
 VMH_Raw[
@@ -50,10 +70,46 @@ VMH_Raw[
     lubridate::days(Transit_Day)
 ]
 
-VMH_Raw[ Stop_Id == 52953, .N, .(Transit_Day, Route,Stop_Id)
-][
-order(Route, Stop_Id, Transit_Day)
-] %>% head(50)
+#okay so this will get raw records
+VMH_Raw[, .N, .(Transit_Day, Route, Stop_Id)
+        ][
+          order(Route, Stop_Id, Transit_Day)
+          ][]
+
+VMH_Raw[, .(Boards = sum(Boards)
+            , Alights = sum(Alights)
+            )
+        , Stop_Id
+        ]
+
+VMH_Raw[, .N, .(Transit_Day, Route, Stop_Id)
+        ][
+          order(Route, Stop_Id, Transit_Day)
+          ][, `:=` (days_to_next = shift(Transit_Day,-1)-Transit_Day
+                    , N_Difference = shift(N,-1)-N
+                    
+                    )
+            , .(Route, Stop_Id)
+            ][Stop_Id != 0
+              ] %>% View()
+
+
+[,.N,days_to_next
+                ]
+
+
+
+[Stop_Id != 0
+              ][!is.na(days_to_next) & 
+                  days_to_next != 1
+                , .N
+                , .(days_to_next,Stop_Id)
+                ]
+
+
+
+
+
 
 data.table(grp = rep(c("A", "B", "C", "A", "B")
                      , c(2, 2, 3, 1, 2)
